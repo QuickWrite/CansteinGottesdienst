f072e64b0d247ad2b23c641cac3c9a411a73ea4b
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2022-03-31 20:58:07 +0200

    Initialize Plugin
    Just creating the base plugin.

---
+++ src/main/java/net/quickwrite/cansteingottesdienst/CansteinGottesdienst.java
@@ -0,0 +1,17 @@
+package net.quickwrite.cansteingottesdienst;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CansteinGottesdienst extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
