176264028d8d13c9b2ea379219e4436ee2a6e048
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2022-04-12 19:18:57 +0200

    Add all custom items

--- src/main/java/net/quickwrite/cansteingottesdienst/items/Items.java
+++ src/main/java/net/quickwrite/cansteingottesdienst/items/Items.java
@@ -7,8 +7,16 @@ import org.bukkit.inventory.ItemStack;
 
 public enum Items {
 
-    BREAD(new ItemBuilder(Material.BREAD).setDisplayName("§6Bread").build()),
-    OX_TONGUE(new ItemBuilder(Material.DANDELION).setDisplayName("§2Bitterkraut").setCustomModelData(1).build())
+    BREAD(new ItemBuilder(Material.BREAD).setDisplayName("§6Brot").build()),
+    CELERY(new ItemBuilder(Material.CARROT).setDisplayName("§bSellerie").setCustomModelData(1).build()),
+    COOKED_LAMB_GIGOT(new ItemBuilder(Material.COOKED_MUTTON).setDisplayName("§eGekochte Lammkeule").setCustomModelData(1).build()),
+    EMPTY_WINE_BOTTLE(new ItemBuilder(Material.BUCKET).setDisplayName("§5Leere Weinflasche").setCustomModelData(1).build()),
+    FLOUR(new ItemBuilder(Material.SUGAR).setDisplayName("§6Mehl").setCustomModelData(1).build()),
+    GRAPES(new ItemBuilder(Material.SWEET_BERRIES).setDisplayName("§5Trauben").setCustomModelData(1).build()),
+    LAMB_GIGOT(new ItemBuilder(Material.MUTTON).setDisplayName("§eLammkeule").setCustomModelData(1).build()),
+    OX_TONGUE(new ItemBuilder(Material.DANDELION).setDisplayName("§2Bitterkraut").setCustomModelData(1).build()),
+    OX_TONGUE_POWDER(new ItemBuilder(Material.APPLE).setDisplayName("§2Bitterkraut Pulver").setCustomModelData(1).build()),
+    WINE(new ItemBuilder(Material.MILK_BUCKET).setDisplayName("§5Wein").setCustomModelData(1).build())
     ;
 
     private final ItemStack stack;
